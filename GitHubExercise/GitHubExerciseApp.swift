7aa88bb60026657c68bd1c8849d61127caba8ddf
Oleksiy Muzalyev <oleksiy.muzalyev@gmail.com>
2024-02-29 15:19:45 +0100

    Initial Commit

---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by Oleksiy Muzalyev on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
